359442c69cf655e094ee4e84714c9d04d01129d2
Йоан Гаврилов <YAGavrilov19@codingburgas.bg>
2023-03-21 15:12:06 +0200

    Add Three Layer Architecture

---
+++ Panopticon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Panopticon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
